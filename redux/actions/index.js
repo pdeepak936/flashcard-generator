373c8b303eda9e32d12f251b470df843ae0c28c6
pdeepak936 <67180496+pdeepak936@users.noreply.github.com>
2023-06-11 23:16:49 +0530

    Delete redux directory

--- redux/actions/index.js
+++
@@ -1,4 +0,0 @@
-import { addGroupId, addCardId } from './link';
-import { removeCard, addCard } from './card';
-import{addRef} from './reference';
-export { removeCard, addCard, addGroupId, addCardId, addRef }
--- redux/actions/reference.js
+++
@@ -1,8 +0,0 @@
-import { ADD_REF } from '../../constants/actionTypes';
-
-export const addRef = (val) => {
-    return {
-        type: 'ADD_REF',
-        payload: val
-    }
-}
